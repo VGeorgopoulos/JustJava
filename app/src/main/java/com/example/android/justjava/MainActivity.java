d37727f47ffb8620baef21c75e072cfaaa720891
Vasilis Georgopoulos <vgeorgopoulosit@gmail.com>
2016-10-19 18:55:33 +0300

    Localization

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Toast;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 1;
+    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
